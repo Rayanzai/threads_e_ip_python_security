3a86589421ab0d61c227b0e85b2076b266cf3c80
Rayan <anzairayan@gmail.com>
2022-07-12 11:28:07 -0300

    Modificação da função e multithreading ativado usando a biblioteca threading

--- threads.py
+++ threads.py
@@ -1,14 +1,14 @@
-def carro1(velocidade):
+import time
+from threading import Thread
+
+def carro(velocidade, piloto):
     trajeto = 0 
     while trajeto <= 100:
-        print("Carro 1: ", trajeto)
         trajeto += velocidade
+        time.sleep(0.5)
+        print("Piloto: {} KM: {} \n".format(piloto, trajeto))   
+t_carro1 = Thread(target=carro, args=[1, "Rayan"])
+t_carro2 = Thread(target=carro, args=[1.5, "Python"])
 
-def carro2(velocidade):
-    trajeto = 0 
-    while trajeto <= 100:
-        print("Carro 2: ", trajeto)
-        trajeto += velocidade
-
-carro1(10)
-carro2(20)
+t_carro1.start()
+t_carro2.start()
